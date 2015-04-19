7dad1425777b7c820a5596924be35feb01e841b3
xinhuangnyc@gmail.com <xinhuangnyc@gmail.com>
2015-04-19 16:56:42 -0500

    budda and butterfly

--- hw4.m
+++ hw4.m
@@ -3,6 +3,7 @@ clear all
 format long
 clc
 
+%% Initialize environment by enabling vlfeat
 current_pwd = pwd; % Get working directory
 clear_pwd = '/storage-home/x/xyh1';
 if (strcmp(current_pwd(1:20),clear_pwd) == 1)
@@ -14,5 +15,40 @@ else
 	'Error; could not utilize VLFeat!\n'
 end
 
+%% Buddas from Reduced Training Dataset
+budda_dir = 'midterm_data/midterm_data_reduced/TrainingDataset/022.buddha-101/';
+budda_struct = struct();
 % Get all .jpg images from directory
-dir('midterm_data/midterm_data_reduced/TrainingDataset/022.buddha-101/*.jpg')
+all_buddas = dir(strcat(budda_dir,'*.jpg'));
+num_buddas = size(all_buddas); % number of budda images
+
+for fileNum = 1:num_buddas(1) % apply to all budda images
+	filename = all_buddas(fileNum).name;
+	budda_image = imread(strcat(budda_dir,filename));
+	if size(budda_image, 3) > 1 % make sure image is not already gray
+		budda_imageG = im2single(rgb2gray(budda_image));
+	end
+	[f,d] = vl_sift(budda_imageG); % frames, descriptors
+	pattern = '.jpg';
+	replacement = '';
+	filename = regexprep(filename,pattern,replacement);
+	budda_struct.(filename) = d;
+end
+
+%% Butterfly from Reduced Training Dataset
+butterfly_dir = 'midterm_data/midterm_data_reduced/TrainingDataset/022.buddha-101/';
+butterfly_struct = struct();
+% Get all .jpg images from directory
+all_butterflies = dir(strcat(butterfly_dir,'*.jpg'));
+num_butterflies = size(all_butterflies); % number of budda images
+
+for fileNum = 1:num_butterflies(1) % apply to all budda images
+	filename = all_butterflies(fileNum).name;
+	butterfly_image = im2single(rgb2gray(imread(strcat(butterfly_dir,filename))));
+	if size(butterfly_image, 3) > 1 % make sure image is not already gray
+		butterfly_imageG = im2single(rgb2gray(butterfly_image));
+	end
+	[f,d] = vl_sift(butterfly_image); % frames, descriptors
+	[pathstr,name,ext] = fileparts(filename)
+	butterfly_struct.(name) = d;
+end
